1ec44d35e69983f1e4c3f59d5555543819e60665
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2024-10-19 02:19:27 +0900

    aa

--- test.py
+++ test.py
@@ -3,35 +3,38 @@ import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 
-# API Keyをセット
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = st.secrets["gemini_key"]
+# Show title and description.
+st.title("💬 Simple Chatbot")
+st.write("This is a simple chatbot that uses several models to generate responses. ")
+st.write("If you want to use OpenAI model, you need to input your API Key.")
+st.write("If you want to use OpenSorce LLM model, you can use without your key.")
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-    # other params...
-)
+# API Keyをセット
+#os.environ["GOOGLE_API_KEY"] = st.secrets["gemini_key"]
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDOkxdeIGiGBWnn93c-znm89dLZ5yf0flM"
+
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash"
+    )
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display the existing chat messages via `st.chat_message`.
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with st.chat_message(message[0]):
+        st.markdown(message[1])
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
-if prompt := st.text_input("What is up?"):
+if prompt := st.chat_input("What is up?"):
 
     # Store and display the current prompt.
-    st.session_state.messages.append(("user", prompt))
+    st.session_state.messages.append(["user", prompt])
     with st.chat_message("user"):
         st.markdown(prompt)
 
@@ -39,4 +42,5 @@ if prompt := st.text_input("What is up?"):
     # session state.
     with st.chat_message("assistant"):
         ai_msg = llm.invoke(st.session_state.messages)
-    st.session_state.messages.append(("assistant",ai_msg.content))
+        st.markdown(ai_msg.content)
+    st.session_state.messages.append(["assistant",ai_msg.content])
